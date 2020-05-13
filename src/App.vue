b5d0c6afde266153d8785d4999d7ee724e364790
zl7261 <zhenliu7261@gmail.com>
2020-05-13 23:59:46 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<!--<template>-->
+<!--    <div id="app">-->
+<!--        {{message}}-->
+<!--    </div>-->
+<!--</template>-->
+
+<script>
+
+export default {
+    name: 'App',
+    created() {
+        console.log(this._data.message)
+    },
+    data() {
+        return {
+            message: 'Hello World!'
+        }
+    },
+    render(createElement) {
+        return createElement('div', {
+        }, [this.message])
+    }
+}
+</script>
