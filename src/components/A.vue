09b07a087e823cf472f338a43d68837497602be8
zl7261 <zhenliu7261@gmail.com>
2020-05-19 23:07:08 +0800

    组件循环依赖

---
+++ src/components/A.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        {{ text }}
+        <B/>
+    </div>
+</template>
+
+<script>
+import B from './B.vue'
+
+export default {
+    name: "A",
+    components: {B},
+    data() {
+        return {
+            text: 'A-Component'
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/B.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        {{ text }}
+        <A/>
+    </div>
+</template>
+
+<script>
+import A from './A.vue'
+
+export default {
+    name: "B",
+    components: {A},
+    data() {
+        return {
+            text: 'B-Component'
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
