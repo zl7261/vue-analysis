f69348315c093632a3848c82f0b1a99b6895f5f1
zl7261 <zhenliu7261@gmail.com>
2020-05-19 23:07:02 +0800

    引用组件的 init过程

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
-<!--<template>-->
-<!--    <div id="app">-->
-<!--        {{message}}-->
-<!--    </div>-->
-<!--</template>-->
+<template>
+    <div id="app">
+        <strong>{{message}}</strong>
+        <HelloWorld/>
+        <img src="./assets/logo.png" width="200" height="200" alt="logo">
+        <B/>
+        <hr>
+        <A/>
+    </div>
+</template>
 
 <script>
+import HelloWorld from './components/HelloWorld'
+// 组件实际加载依赖 跟import的顺序有关系
+import A from './components/A'
+import B from './components/B'
 
 export default {
     name: 'App',
+    components: {HelloWorld, A, B},
     created() {
         // console.log(this._data.message)
     },
@@ -18,8 +28,16 @@ export default {
     },
     render(createElement) {
         return createElement('div', {
-            attrs:{
-                id:'app'
+            attrs: {
+                id: 'app',
+                haha: ''
+            },
+            class: [
+                {'cls1': true},
+                {'cls2': false}
+            ],
+            style: {
+                color: 'red'
             }
         }, [this.message])
     }
